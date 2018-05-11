66580342180879f0307c4734f132a820fc60f97d
PRbsas <paularamirez0316@gmail.com>
2018-05-11 14:20:54 -0400

    Set initial values for form fields

--- src/App.js
+++ src/App.js
@@ -5,9 +5,16 @@ class RegisterFormContainer extends Component {
   submit = values => {
     window.alert(JSON.stringify(values, null, 4))
   }
+
+  getInitialValues () {
+    return {
+      preference: 'spaces',
+      newsletter: true
+    }
+  }
   render () {
     return (
-      <RegisterForm onSubmit={this.submit} />
+      <RegisterForm onSubmit={this.submit} initialValues={this.getInitialValues()} />
     )
   }
 }
